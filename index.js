be518269f198bd6f97b38ec7e12bf769d6d93fb2
gtretow <guilherme.tretow@gmail.com>
2021-02-12 02:19:40 -0300

    ajustes

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello to memories API");
+});
+
+const PORT = process.env.PORT;
 
 //prevenindo warnings
 mongoose
